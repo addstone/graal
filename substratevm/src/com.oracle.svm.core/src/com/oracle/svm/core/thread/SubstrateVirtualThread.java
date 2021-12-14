2bd7ba636cb33520d85e55f6e91cee8dfcc8c4d2
Peter Hofer <peter.hofer@oracle.com>
2021-12-14 16:16:30 +0100

    Support pinning and do not permit yield with native frames.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/thread/SubstrateVirtualThread.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/thread/SubstrateVirtualThread.java
@@ -41,6 +41,8 @@ import org.graalvm.compiler.serviceprovider.GraalUnsafeAccess;
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.TargetClass;
 import com.oracle.svm.core.jdk.Package_jdk_internal_misc;
+import com.oracle.svm.core.stack.JavaFrameAnchor;
+import com.oracle.svm.core.stack.JavaFrameAnchors;
 import com.oracle.svm.core.util.VMError;
 
 import sun.misc.Unsafe;
@@ -92,6 +94,9 @@ final class SubstrateVirtualThread extends Thread {
 
     // carrier thread when mounted
     private volatile Thread carrierThread;
+
+    // number of active pinnings to the carrier thread
+    private short pins;
 
     // termination object when joining, created lazily if needed
     private volatile CountDownLatch termination;
@@ -184,6 +189,15 @@ final class SubstrateVirtualThread extends Thread {
     }
 
     private boolean yieldContinuation() {
+        assert this == Thread.currentThread();
+        if (pins > 0) {
+            return false;
+        }
+        JavaFrameAnchor anchor = JavaFrameAnchors.getFrameAnchor();
+        if (anchor.isNonNull() && cont.sp.aboveThan(anchor.getLastJavaSP())) {
+            return false;
+        }
+
         unmount();
         try {
             return cont.yield() == JavaContinuations.YIELD_SUCCESS;
@@ -592,6 +606,24 @@ final class SubstrateVirtualThread extends Thread {
 
     private void dispatchUncaughtException(Throwable e) {
         getUncaughtExceptionHandler().uncaughtException(this, e);
+    }
+
+    void pin() {
+        assert currentThread() == this;
+        assert pins >= 0;
+        if (pins == Short.MAX_VALUE) {
+            throw new IllegalStateException("Too many pins");
+        }
+        pins++;
+    }
+
+    void unpin() {
+        assert currentThread() == this;
+        assert pins >= 0;
+        if (pins == 0) {
+            throw new IllegalStateException("Not pinned");
+        }
+        pins--;
     }
 
     @Override
